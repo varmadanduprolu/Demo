7444a5cf41f534f301b841fb98d4c6ad8ab08ffc
Madhav <varmadhanduprolu@gmail.com>
2023-08-11 18:32:08 +0530

    2changes

--- src/main/java/com/pallavi/questionProject/repository/QuestionRepository.java
+++ src/main/java/com/pallavi/questionProject/repository/QuestionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     public Question findQuestionById(Integer id);
+    public boolean existsQuestionById(Integer id);
 
 }
